54dca6f526854dcaccc29a4358270f7468405e37
monpro <monproshen@gmail.com>
2022-05-02 21:50:54 +1000

    add getUserSubscribedMoments api
    https://monproshen.atlassian.net/browse/SPD-27

--- MonProVideoStreamApi/src/main/java/com/monpro/api/UserMomentApi.java
+++ MonProVideoStreamApi/src/main/java/com/monpro/api/UserMomentApi.java
@@ -6,11 +6,12 @@ import com.monpro.domain.UserMoment;
 import com.monpro.service.UserMomentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -28,5 +29,12 @@ public class UserMomentApi {
     userMoment.setUserId(userId);
     userMomentsService.addUserMoments(userMoment);
     return JsonResponse.success();
+  }
+
+  @GetMapping("/user-subscribed-moments")
+  public JsonResponse<List<UserMoment>> getUserSubscribedMoments() {
+    final Long userId = userSupport.getCurrentUserId();
+    final List<UserMoment> list = userMomentsService.getUserSubscribedMoments(userId);
+    return new JsonResponse<>(list);
   }
 }
--- MonProVideoStreamService/src/main/java/com/monpro/service/UserMomentService.java
+++ MonProVideoStreamService/src/main/java/com/monpro/service/UserMomentService.java
@@ -1,5 +1,6 @@
 package com.monpro.service;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.monpro.dao.UserMomentDao;
 import com.monpro.domain.UserMoment;
@@ -10,10 +11,12 @@ import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.common.message.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -25,11 +28,20 @@ public class UserMomentService {
   @Autowired
   private ApplicationContext applicationContext;
 
+  @Autowired
+  private RedisTemplate<String, String> redisTemplate;
+
   public void addUserMoments(UserMoment userMoment) throws Exception {
     userMoment.setCreateTime(new Date());
     userMomentDao.addUserMoments(userMoment);
     final DefaultMQProducer producer = (DefaultMQProducer) applicationContext.getBean("momentsProducer");
     final Message message = new Message(UserMomentConstant.MOMENTS_TOPIC, JSONObject.toJSONString(userMoment).getBytes(StandardCharsets.UTF_8));
     RocketMQUtil.syncSendMsg(producer, message);
+  }
+
+  public List<UserMoment> getUserSubscribedMoments(final Long userId) {
+    final String key = "subscribe-" + userId;
+    final String listString = redisTemplate.opsForValue().get(key);
+    return JSONArray.parseArray(listString, UserMoment.class);
   }
 }
