e997060b1b411924073ac7e85a11e66504d827d3
monpro <monproshen@gmail.com>
2022-05-02 16:40:53 +1000

    momentsConsumer consume msg by saving it too all fans redis
    https://monproshen.atlassian.net/browse/SPD-26

--- MonProVideoStreamService/src/main/java/com/monpro/service/config/RocketMQConfig.java
+++ MonProVideoStreamService/src/main/java/com/monpro/service/config/RocketMQConfig.java
@@ -1,6 +1,13 @@
 package com.monpro.service.config;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.monpro.domain.UserFollowing;
+import com.monpro.domain.UserMoment;
+import com.monpro.service.UserFollowingService;
+import io.netty.util.internal.StringUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
@@ -14,6 +21,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.monpro.domain.constant.UserMomentConstant.MOMENTS_GROUP;
@@ -28,6 +36,9 @@ public class RocketMQConfig {
 
   @Autowired
   private RedisTemplate<String, String> redisTemplate;
+
+  @Autowired
+  private UserFollowingService userFollowingService;
 
   @Bean("momentsProducer")
   public DefaultMQProducer momentsProducer() throws MQClientException {
@@ -45,9 +56,26 @@ public class RocketMQConfig {
     consumer.registerMessageListener(new MessageListenerConcurrently() {
       @Override
       public ConsumeConcurrentlyStatus consumeMessage(final List<MessageExt> list, final ConsumeConcurrentlyContext consumeConcurrentlyContext) {
-        for(final MessageExt msg: list) {
-          log.info(msg.toString());
+        final MessageExt messageExt = list.get(0);
+        if (messageExt == null) {
+          return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
         }
+        final String bodyString = new String(messageExt.getBody());
+        final UserMoment userMoment = JSONObject.toJavaObject(JSONObject.parseObject(bodyString), UserMoment.class);
+        final Long userId = userMoment.getUserId();
+        final List<UserFollowing> userFans = userFollowingService.getUserFans(userId);
+        userFans.forEach(userFan -> {
+          final String key = "subscribe-" + userFan.getUserId();
+          final String subscribeListString = redisTemplate.opsForValue().get(key);
+          final List<UserMoment> subscribeList;
+          if (StringUtil.isNullOrEmpty(subscribeListString)) {
+            subscribeList = new ArrayList<>();
+          } else {
+            subscribeList = JSONArray.parseArray(subscribeListString, UserMoment.class);
+          }
+          subscribeList.add(userMoment);
+          redisTemplate.opsForValue().set(key, JSONObject.toJSONString(subscribeList));
+        });
         return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
       }
     });
